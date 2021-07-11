262999db1c7d91fac1bfff8a97876d388747d268
saniag081 <sgonzalezhernandez081@gmail.com>
2021-07-10 22:44:59 -0500

    hello world with nodeJS

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+	response.writeHead(200, {'Content-type': 'text/plain'})
+	response.end('Hello word')
+})
+
+const PORT = 3001
+
+app.listen(PORT)
+
+console.log(`Server running on port ${PORT}`)
