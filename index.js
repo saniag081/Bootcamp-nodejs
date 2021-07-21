b1f9180bbd2c631bbf155a1fda5d7429492764ea
saniag081 <sgonzalezhernandez081@gmail.com>
2021-07-20 21:03:20 -0500

    delete and post

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-// const http = require('http')
 
+app.use(express.json())
+
+// const http = require('http')
 // const app = http.createServer((request, response) => {
 // 	response.writeHead(200, {'Content-type': 'text/plain'})
 // 	response.end('Hello word')
 // })
 
-const notes = [
+let notes = [
 	{
 		"id": 1,
 		"content": "nota numero 1",
@@ -43,7 +45,30 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 app.delete('/api/notes/:id', (request, response) => {
+	const id = Number(request.params.id)
+	notes = notes.filter(note => note.id !== id)
+	response.status(204).end()
+})
 
+app.post('/api/notes', (request, response) => {
+	const noteRequest = request.body
+	const ids = notes.map((note)=> note.id)
+	const maxId = Math.max(...ids)
+
+	if (!noteRequest || !noteRequest.content) {
+		return response.status(400).json('note.content is missing')
+	}
+
+	const newNote = {
+		id: maxId + 1,
+		content: noteRequest.content,
+		important: typeof noteRequest.important !== 'undefined' ? noteRequest.important : false,
+		date: new Date().toISOString()
+	}
+
+	notes = [...notes, newNote]
+
+	response.json(newNote)
 })
 
 const PORT = 3001
